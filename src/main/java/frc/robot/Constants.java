2ceb050528eddde993aa046f05c3091d3f30b9c6
aogkievit <140564458+aogkievit@users.noreply.github.com>
2025-01-13 17:05:32 -0500

    Added Vision Constants; they are broken, and Abe might fix.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,9 +6,39 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.Measure;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Distance;
+import static edu.wpi.first.units.Units.*;
+
 public final class Constants {
     public final class OperatorInterfaceConstants {
         public final static int kXboxControllerPort = 0;
         public final static int kControlPanelPort = 1;
     }
+
+      public final class VisionConstants {
+    public static final String kPhotonCameraName = "photonvision";
+    public static final String kNoteCameraName = "photon_note";
+
+    // Camera mounted facing forward, half a meter forward of center, half a meter
+    // up from center. TODO: Measure this
+    public static final Transform3d kRobotToCamera = new Transform3d(new Translation3d(0.5, 0.0, 0.5),
+        new Rotation3d(0.0, 0.0, 0.0));
+
+    public static final Measure<Distance> kCameraHeight = Inches.of(10.75);
+    public static final Measure<Angle> kCameraPitch = Degrees.of(20); //23.5
+
+    public static final Matrix<N3, N1> kSingleTagStdDevs = VecBuilder.fill(4, 4, 8);
+    public static final Matrix<N3, N1> kMultiTagStdDevs = VecBuilder.fill(0.5, 0.5, 1);
+  }
+
 }
