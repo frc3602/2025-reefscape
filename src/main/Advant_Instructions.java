a4b2f8fc78905bd5106e00c4544ef119b4166355
carolinef158 <cfurmanski158@gmail.com>
2025-01-28 16:49:42 -0500

    Fixed Advantage scope models and added instuctions (yw abe)

---
+++ src/main/Advant_Instructions.java
@@ -0,0 +1,12 @@
+/*
+
+To use a 3d model in advantage scope, DO NOT use custom assets. 
+1)Open advantage scope
+2) on the left side, go to SmartDashboard, then VisionSystemSim-main, then Sim Field, then drag and drop "Robot" into Poses on a 3D Field. 
+3) Go to help, and click on Show Assets Folder. It should pop up in a file explorer
+4) In the file explorer that pops up, go to this very code space and find the folder "AdvantageScopeAssets", and Robot_3602ReefscapeBot. Copy the Robot_3602ReefscapeBot folder and hit the back arrow to go back to the original folder that popped up and paste it there. 
+5) Back in advantage scope, click on the arrow symbol to the right of the current robot model name in your poses. Click on model and select the 3602 one. 
+
+If you make changes to the model, config or add components or anything like that, you have to go back to "show assets folder", delete the folder we added, and readd it
+
+*/
