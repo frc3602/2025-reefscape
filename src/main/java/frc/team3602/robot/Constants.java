ec2e6b173c00f7707ff0c7ca801fe873982ff2ed
carolinef158 <cfurmanski158@gmail.com>
2025-02-03 23:58:35 -0500

    Used 4 sim cams(that should translate to real life) + drivetrain to give estimated pose (I believe)

--- src/main/java/frc/team3602/robot/Constants.java
+++ src/main/java/frc/team3602/robot/Constants.java
@@ -14,6 +14,7 @@ import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.units.AngleUnit;
 import edu.wpi.first.units.DistanceUnit;
 import edu.wpi.first.units.Measure;
